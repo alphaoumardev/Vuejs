b405201eaf0c2bd2f26b776cc8f1ca6533b67078
oumar <oumardialo98@qq.com>
2021-08-19 22:39:56 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports=
+    {
+        devServer:
+            {
+                proxy:
+                    {
+                        '^api':
+                            {
+                                target: 'http://localhost:3000',
+                                changeOrigin: true,
+                                logLevel: 'debug',
+                                pathRewrite:{'^api/':'/'}
+                            }
+                    }
+            }
+    }
